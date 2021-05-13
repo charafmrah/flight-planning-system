ae757498dffdaeb991086d5353982d69509b6561
Charaf Mrah <charaf4charaf@gmail.com>
2021-05-14 00:46:01 +0300

    Added City class

--- app/src/main/java/com/example/aviationsystem/Node.java
+++ app/src/main/java/com/example/aviationsystem/Node.java
@@ -1,16 +1,40 @@
 package com.example.aviationsystem;
 
 public class Node {
-    private Edge edge;
-    public Node next;
 
-    public Node(Edge edge) {
-        this.edge = edge;
+    private City city;
+    private Node next;
+
+    public Node() {
+        city = null;
         next = null;
     }
 
-    public Edge getEdge() {
-        return edge;
+    public Node(City city) {
+        this.city = city;
+        next = null;
     }
 
+    public Node(City city, Node next) {
+        this.next = next;
+        this.next = next;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
 }
+
