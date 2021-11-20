333ff3d49d4de8bb15f8f0ac24412bdebe768068
Charaf Mrah <charaf4charaf@gmail.com>
2021-11-20 16:36:49 +0300

    added navigation

---
+++ app/src/main/java/com/example/aviationsystem/fragments/HomepageFragment.java
@@ -0,0 +1,11 @@
+package com.example.aviationsystem.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.aviationsystem.R;
+
+public class HomepageFragment extends Fragment {
+    public HomepageFragment() {
+        super(R.layout.fragment_homepage);
+    }
+}
