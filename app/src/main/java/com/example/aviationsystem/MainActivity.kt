aed2002fbd983f9543cb6c8cb950c026603e40f8
Charaf Mrah <charaf4charaf@gmail.com>
2021-11-20 18:01:29 +0300

    the homepage fragment now shows on launch

---
+++ app/src/main/java/com/example/aviationsystem/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.aviationsystem
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(this, navController)
+    }
+
+}
--- app/src/main/java/com/example/aviationsystem/fragments/HomepageFragment.kt
+++ app/src/main/java/com/example/aviationsystem/fragments/HomepageFragment.kt
@@ -1,6 +1,30 @@
 package com.example.aviationsystem.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.aviationsystem.databinding.FragmentHomepageBinding
 
 class HomepageFragment : Fragment() {
+
+    private var _binding: FragmentHomepageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?,
+    ) : View? {
+        _binding = FragmentHomepageBinding.inflate(inflater, container, false)
+        Log.e("HomePage", "OnCreateView called")
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
