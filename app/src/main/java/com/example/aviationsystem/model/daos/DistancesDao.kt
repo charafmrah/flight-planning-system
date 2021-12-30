608cd8d32e0cdb99e4d2fc5908990131f4526a8b
Charaf Mrah <charaf4charaf@gmail.com>
2021-12-30 19:22:13 +0300

    implemented one-to-many relationship

---
+++ app/src/main/java/com/example/aviationsystem/model/daos/DistancesDao.kt
@@ -0,0 +1,20 @@
+package com.example.aviationsystem.model.daos
+
+import androidx.room.*
+import com.example.aviationsystem.model.entities.Distances
+import com.example.aviationsystem.model.entities.Route
+import com.example.aviationsystem.model.relations.DistancesWithRoute
+
+@Dao
+interface DistancesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertDistance(distance: Distances)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRoute(route: Route)
+
+    @Transaction
+    @Query("SELECT * FROM distances WHERE distanceId = :distancesId")
+    suspend fun getDistancesWithRoute(distancesId: Int): List<DistancesWithRoute>
+}
