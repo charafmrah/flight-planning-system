c3f562d1d0e86da2e59bf61914bf7b2fa5cb0031
Charaf Mrah <charaf4charaf@gmail.com>
2022-02-17 19:29:10 +0100

    implemented the add page

---
+++ app/src/main/java/com/example/aviationsystem/fragments/AddpageFragment.kt
@@ -0,0 +1,36 @@
+package com.example.aviationsystem.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.aviationsystem.databinding.FragmentHomepageBinding
+import com.example.aviationsystem.City
+import com.example.aviationsystem.databinding.FragmentAddpageBinding
+import com.example.aviationsystem.datastructures.Edge
+import com.example.aviationsystem.datastructures.Graph
+
+
+class AddpageFragment : Fragment() {
+
+    private var _binding: FragmentAddpageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ) : View? {
+        _binding = FragmentAddpageBinding.inflate(inflater, container, false)
+        Log.e("HomePage", "OnCreateView called")
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
