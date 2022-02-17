9d657eadf4c5963e4e25419cd18bc85466b91e8a
Charaf Mrah <charaf4charaf@gmail.com>
2022-02-17 20:34:14 +0100

    implemented the bottom navigation, it works now

--- app/src/main/java/com/example/aviationsystem/MainActivity.kt
+++ app/src/main/java/com/example/aviationsystem/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.aviationsystem
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -13,12 +16,20 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Retrieve NavController from the NavHostFragment
+
+
+//        // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
                 .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        // Set up the action bar for use with the NavController
-        setupActionBarWithNavController(this, navController)
+//        // Set up the action bar for use with the NavController
+////        setupActionBarWithNavController(this, navController)
+
+//        val navController: NavController =
+//            Navigation.findNavController(this, R.id.nav_host_fragment)
+        val bottomNavigationView =
+            findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 
 }
